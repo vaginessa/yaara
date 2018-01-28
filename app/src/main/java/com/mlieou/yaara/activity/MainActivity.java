72898073b76a4626745b01ea1d8df89bbb1c6588
mlieou <ifddd@outlook.com>
2018-01-28 17:05:58 -0500

    - close drawer when click server - remove preference summary for server editor

--- app/src/main/java/com/mlieou/yaara/activity/MainActivity.java
+++ app/src/main/java/com/mlieou/yaara/activity/MainActivity.java
@@ -127,7 +127,7 @@ public class MainActivity extends AppCompatActivity implements HandlerCallback, 
                             mServerProfileManager.setActiveServerId(id);
                             reloadServerProfile();
                         }
-                        return true;
+                        return false;
                     }
                 })
 
