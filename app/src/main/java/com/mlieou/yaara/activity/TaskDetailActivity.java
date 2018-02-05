82087d7a248905cb5a6f305c330a269d4992652c
mlieou <ifddd@outlook.com>
2018-02-04 21:36:34 -0500

    create views for detail task

---
+++ app/src/main/java/com/mlieou/yaara/activity/TaskDetailActivity.java
@@ -0,0 +1,15 @@
+package com.mlieou.yaara.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.mlieou.yaara.R;
+
+public class TaskDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_detail);
+    }
+}
---
+++ app/src/main/java/com/mlieou/yaara/fragment/TaskDetailTrackerFragment.java
@@ -0,0 +1,10 @@
+package com.mlieou.yaara.fragment;
+
+import android.app.Fragment;
+
+/**
+ * Created by mlieou on 2/4/18.
+ */
+
+public class TaskDetailTrackerFragment extends Fragment {
+}
