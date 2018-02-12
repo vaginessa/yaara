46172f1171776f6a2f209a1809cbb0e9f74c224d
mlieou <ifddd@outlook.com>
2018-02-12 16:14:19 -0500

    - block fragment is now working

--- app/src/main/java/com/mlieou/yaara/fragment/TaskDetailBlockFragment.java
+++ app/src/main/java/com/mlieou/yaara/fragment/TaskDetailBlockFragment.java
@@ -9,17 +9,40 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.mlieou.yaara.R;
+import com.mlieou.yaara.model.TaskStatus;
+import com.mlieou.yaara.widget.BitFieldView;
+
+import java.util.Observable;
+import java.util.Observer;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by mlieou on 2/10/18.
  */
 
-public class TaskDetailBlockFragment extends Fragment {
+public class TaskDetailBlockFragment extends Fragment implements Observer {
+
+    @BindView(R.id.bfv_block_map)
+    BitFieldView blockMap;
+    int pieces = 0;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_blocks, container, false);
+        ButterKnife.bind(this, view);
         return view;
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        TaskStatus status = (TaskStatus) arg;
+        if (status.getNumPieces() != pieces) {
+            pieces = status.getNumPieces();
+            blockMap.setPieces(pieces);
+        }
+        blockMap.setBitField(status.getBitfield());
     }
 }
--- app/src/main/java/com/mlieou/yaara/widget/BitFieldView.java
+++ app/src/main/java/com/mlieou/yaara/widget/BitFieldView.java
@@ -18,6 +18,9 @@ import com.mlieou.yaara.R;
 public class BitFieldView extends View {
 
     private String bitField;
+    private int pieces;
+    private int BLOCKS_PER_LINE = 20;
+    private char lastBlock;
     private Drawable square = getResources().getDrawable(R.drawable.download_block);
 
     public BitFieldView(Context context, @Nullable AttributeSet attrs) {
@@ -25,22 +28,79 @@ public class BitFieldView extends View {
     }
 
     @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int rows = calculateNumOfBlocks(pieces) / BLOCKS_PER_LINE + 1;
+        int width = MeasureSpec.getSize(widthMeasureSpec);
+        setMeasuredDimension(width, width / BLOCKS_PER_LINE * rows);
+    }
+
+    @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        int size = getWidth() / 20;
-        for (int x = 0; x < 20; x++) {
-            for (int y = 0; y < 2; y++) {
-                square.setBounds(x * size, y * size, (x + 1) * size, (y + 1) * size);
-                square.setColorFilter(Color.BLUE, PorterDuff.Mode.SRC_ATOP);
-                square.draw(canvas);
+        int width = getWidth();
+        int blockSize = width / BLOCKS_PER_LINE;
+        int offset = (width - blockSize * BLOCKS_PER_LINE) / 2;
+        int blocks = calculateNumOfBlocks(pieces);
+        int i = 0;
+        while (i < blocks) {
+            square.setBounds(i % BLOCKS_PER_LINE * blockSize + offset,
+                    i / BLOCKS_PER_LINE * blockSize,
+                    (i % BLOCKS_PER_LINE + 1) * blockSize + offset,
+                    (i / BLOCKS_PER_LINE + 1) * blockSize);
+            if (bitField.charAt(i) == 'f' || (i + 1 == blocks && bitField.charAt(i) == lastBlock)) {
+                square.setColorFilter(Color.GREEN, PorterDuff.Mode.SRC_ATOP);
             }
+            else {
+                square.setColorFilter(Color.GRAY, PorterDuff.Mode.SRC_ATOP);
+            }
+            square.draw(canvas);
+            i++;
         }
-
     }
 
     public void setBitField(String bitField) {
-        // Draw UI
+        if (bitField.equals(this.bitField))
+            return;
         this.bitField = bitField;
         invalidate();
     }
+
+    public void setPieces(int pieces) {
+        this.pieces = pieces;
+        invalidate();
+        requestLayout();
+    }
+
+    private int calculateNumOfBlocks(int pieces) {
+        int blocks = pieces / 8 * 2;
+        if (pieces % 8 > 0) {
+            int n;
+            if ((pieces - blocks * 4) > 4) {
+                n = (pieces - blocks * 4) - 4;
+                blocks += 2;
+            } else {
+                n = pieces - blocks * 4;
+                blocks++;
+            }
+            switch (n) {
+                case 1:
+                    lastBlock = '8';
+                    break;
+                case 2:
+                    lastBlock = 'c';
+                    break;
+                case 3:
+                    lastBlock = 'e';
+                    break;
+                case 4:
+                    lastBlock = 'f';
+            }
+        }
+        else {
+            lastBlock = 'f';
+        }
+        return blocks;
+    }
+
 }
