b697d0b1b46718256df4f9c231a48073693cc68f
mlieou <ifddd@outlook.com>
2018-02-14 22:14:34 -0500

    add tracker list placeholder

--- app/src/main/java/com/mlieou/yaara/fragment/TaskDetailTrackerFragment.java
+++ app/src/main/java/com/mlieou/yaara/fragment/TaskDetailTrackerFragment.java
@@ -5,14 +5,15 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.mlieou.yaara.R;
 import com.mlieou.yaara.adapter.TaskDetailTrackerAdapter;
 import com.mlieou.yaara.model.TaskStatus;
+import com.mlieou.yaara.widget.PlaceholderRecyclerView;
 
 import java.util.Observable;
 import java.util.Observer;
@@ -29,8 +30,10 @@ public class TaskDetailTrackerFragment extends Fragment implements Observer {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_tracker_list, container, false);
-        RecyclerView recyclerView = view.findViewById(R.id.recycler_view_tracker_list);
+        PlaceholderRecyclerView recyclerView = view.findViewById(R.id.recycler_view_tracker_list);
+        LinearLayout placeHolder = view.findViewById(R.id.empty_list_placeholder);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setEmptyView(placeHolder);
         mAdapter = new TaskDetailTrackerAdapter();
         recyclerView.setAdapter(mAdapter);
         return view;
