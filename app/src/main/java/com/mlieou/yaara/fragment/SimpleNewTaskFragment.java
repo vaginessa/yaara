ed19ca0aaf7b5ff3e5b574d3b1ec5913eb7390f4
mlieou <ifddd@outlook.com>
2018-03-07 15:28:56 -0500

    use v4 dialogFragment instead

--- app/src/main/java/com/mlieou/yaara/fragment/SimpleNewTaskFragment.java
+++ app/src/main/java/com/mlieou/yaara/fragment/SimpleNewTaskFragment.java
@@ -2,8 +2,8 @@ package com.mlieou.yaara.fragment;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.app.DialogFragment;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.view.View;
 import android.widget.EditText;
 
